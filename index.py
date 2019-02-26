1844b6541c70db6da6cf70c69da4703e894a9f72
Dilip <ddiliproyal@hotmail.com>
2019-02-26 11:06:47 +0530

    Globals practice

--- index.py
+++ index.py
@@ -1,10 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, g
 
 app = Flask(__name__)
 
+@app.before_request
+def authenticate():
+    if request.authorization:
+        g.user = request.authorization["username"]
+    else:
+        g.user = "Anonymous"
+
 @app.route("/api")
 def my_microservice():
-    return jsonify({"Hello": "World!!!"})
+    response = jsonify({"Hello": g.user})
+    return response
 
 
 if __name__ == "__main__":
